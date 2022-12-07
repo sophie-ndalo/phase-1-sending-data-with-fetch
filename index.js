16c143e101d1e9a18bc9c075651d3523ae575d47
sophia <sophiendalo@gmail.com>
2022-12-07 07:58:21 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Add your code here
+const formData = {
+    name :"Steve",
+    email :"steve@steve.com",
+};
+submitData = {
+    method : "POST",
+    headers :{
+        "Content-Type" : application/jason,
+        Accept : application/json,
+ },
+    body : JSON.stringify(formData)
+
+}
+fetch('http://localhost:3000/users')
+.then(function (response) {
+    return response.json();
+  })
+  .then(function (object) {
+    console.log(object);
+  })
+  .catch(function (error){
+    alert("very unfortunate!");
+    console.log(error.message);
+  });
+  console.log(submitData);
